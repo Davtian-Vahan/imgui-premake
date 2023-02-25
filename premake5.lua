d21320b69adc1b51572186a0ba35129326b2797e
Vahan Davtian <jedavtian@gmail.com>
2023-02-26 01:14:35 +0400

    Premake file added

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+binoutputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+   targetdir ("Binaries/%{prj.name}/Bin-" .. binoutputdir)
+   objdir ("Binaries/%{prj.name}/Bin-Int-" .. binoutputdir)
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
